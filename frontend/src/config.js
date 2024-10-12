6c5be237bedb49560430f241901ea21a157c511d
Shayne Wang <shayne.wx1997@gmail.com>
2024-10-12 22:05:43 +1100

    Update frontend/src/config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://your-heroku-backend-url.herokuapp.com';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://airbrb-shayne-2d6f81fc9a3b.herokuapp.com';
 export default API_BASE_URL;

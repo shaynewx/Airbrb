90811005bd805d7ab34ad7f5e505dbb660e2fc10
Shayne Wang <shayne.wx1997@gmail.com>
2024-10-12 17:06:20 +1100

    Fix port and config file issues for Heroku

--- backend/src/server.js
+++ backend/src/server.js
@@ -260,7 +260,7 @@ app.get('/', (req, res) => res.redirect('/docs'));
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const configData = JSON.parse(fs.readFileSync('../frontend/src/config.json'));
-const port = 'BACKEND_PORT' in configData ? configData.BACKEND_PORT : 5033;
+const port = process.env.PORT || process.env.BACKEND_PORT || 5033;
 
 const server = app.listen(port, () => {
   console.log(`Backend is now listening on port ${port}!`);

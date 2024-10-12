8a1f679b79b1bd8276b19c638ca0c8bb2042b062
Shayne Wang <shayne.wx1997@gmail.com>
2024-10-12 21:20:49 +1100

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,13 @@ import cors from 'cors';
 import morgan from 'morgan';
 import path from 'path';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 import { InputError, AccessError } from './error.js';
 import swaggerDocument from '../swagger.json' assert { type: 'json' };
 
